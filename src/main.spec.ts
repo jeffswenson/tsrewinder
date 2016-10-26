ccc30ce05a72bef04feae5e4bd4bff8bcef805fc
Jeff <jeffpswenson@gmail.com>
2016-10-26 18:42:26 -0500

    Set up build and test tooling.

---
+++ src/main.spec.ts
@@ -0,0 +1,8 @@
+import {assert} from "chai";
+import "mocha";
+
+describe("This is the first test", function() {
+    it("the first case", function() {
+        assert.equal("This is", "This is");
+    });
+});
