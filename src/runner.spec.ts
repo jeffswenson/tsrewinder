3cfba0703e140776d4964dfa897e44080768ebbf
Jeff <jeffpswenson@gmail.com>
2016-11-26 11:10:52 -0600

    Write nodeToFileTransform and runner shell.

--- src/runner.spec.ts
+++ src/runner.spec.ts
@@ -1,8 +1,14 @@
 import {expect} from 'chai';
 import 'mocha';
 
-describe("fileTransform", function() {
+describe("runFileTransform", function() {
     it("Runs the transform on every file in the glob and returns edits", function() {
+        expect(false).to.equal(true);
+    });
+});
+
+describe("nodeToFileTransform", function() {
+    it("Takes a node transform and returns a file transform", function() {
         expect(false).to.equal(true);
     });
 });
--- src/runner.ts
+++ src/runner.ts
@@ -1,12 +1,54 @@
+import * as ts from "typescript";
+import * as glob from "glob";
+
+import {RewinderServiceHost, RewinderCompilerHost} from "./hosts";
+import {findNodes} from "./astutil";
+
+export interface FileTransform {
+    (fileRoot : ts.Node, context : RunnerContext) : ts.TextChange [];
+}
 
 export interface NodeTransform {
-
+    (node : ts.Node, context : RunnerContext) : ts.TextChange [];
 }
 
 export interface RunnerContext {
-
+    languageService : ts.LanguageService,
+    languageHost : ts.LanguageServiceHost,
+    compilerHost : ts.CompilerHost,
+    files : string []
 }
 
-export function runNodeTransform(transform : NodeTransform, runnerContext : RunnerContext) {
-    
+export function createContext(globString : string) : RunnerContext {
+    let files = glob.sync(globString);
+    let compilerHost = new RewinderCompilerHost();
+    let languageHost = new RewinderServiceHost(files, compilerHost);
+    let languageService = ts.createLanguageService(languageHost);
+    return {
+        files,
+        compilerHost,
+        languageHost,
+        languageService
+    }
+}
+
+export function runFileTransform(transform : FileTransform, runnerContext : RunnerContext) {
+    let edits : {[fileName:string]:ts.TextChange[]};
+    for (let file of runnerContext.files) {
+        //TODO Log Edits to the Console
+    }
+}
+
+export function nodeToFileTransform(transform : NodeTransform, nodesMatching : (node : ts.Node) => boolean) : FileTransform {
+    return function(fileRoot : ts.Node, context : RunnerContext) {
+        let edits : ts.TextChange[] = [];
+
+        for (let node of findNodes(fileRoot, nodesMatching)) {
+            for (let edit of transform(node, context)) {
+                edits.push(edit);
+            }
+        }
+
+        return edits;
+    }
 }
